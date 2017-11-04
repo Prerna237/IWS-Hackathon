0996b4a14fb3c32cc6db4bb1f4a9bfdf641ba427
Saketh Vallakatla <saketh21v@gmail.com>
2017-11-04 16:53:07 +0530

    Simple login routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var _Emitter = require('./lib/EventEmitter');
 
@@ -18,13 +19,25 @@ const db = new DB(_Emitter);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+// ======== Express Work ========== //
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // Have to remove this.
+app.use(session({
+    secret: 'Secret', saveUninitialized: false, resave: false
+}));
+
+// ======= Authentication setup ======== //
+
+
+
+// ======= Routes setup ======== //
 
 app.use('/', index);
 app.use('/users', users);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
+'use strict'
+
 var express = require('express');
+var fs = require('fs');
+var _Emitter = require('../lib/EventEmitter');
+var _EVENTS = require('../lib/Constants')._EVENTS;
 var router = express.Router();
 
+router.get('/', (req, res, next) => {
+  next();
+});
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.use('/login', function (req, res, next) {
+  if (req.body.userName == undefined) {
+    var data = fs.readFileSync('./login.html');
+    console.log("Data Sent. ");
+    res.end(data);
+  } else {
+    var userDetails = {};
+    userDetails.userName = req.body.userName;
+    userDetails.password_hash = req.body.password;
+    
+    _Emitter.emit(_EVENTS.CHECK_USER_AUTH, userDetails);
+
+    _Emitter.on(_EVENTS.USER_AUTH_SUCCESS, () => {
+      res.end("Successfully logged in. Mr. " + userDetails.userName);
+    });
+    _Emitter.on(_EVENTS.USER_AUTH_FAIL, () => {
+      res.end("You failed miserably Mr. "+ userDetails.userName);
+    });
+    _Emitter.on(_EVENTS.NO_SUCH_USER, () => {
+      res.end("You dont exist. Begone.");
+    });
+  }
 });
 
 module.exports = router;
