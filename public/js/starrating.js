79763e2378ec598097b6e13f69857ab7fc7e4f41
Saketh Vallakatla <saketh21v@gmail.com>
2017-11-16 15:48:08 +0530

    Added date in thread

--- public/js/starrating.js
+++ public/js/starrating.js
@@ -1,9 +1,17 @@
-(function(d, t, e, m){
+(function (d, t, e, m) {
 
     // Async Rating-Widget initialization.
-    window.RW_Async_Init = function(){
+    window.RW_Async_Init = function () {
 
         RW.init({
+            beforeRate: (rating, vote) => {
+                alert("Checking");
+                if (!Cookies.get('userName')) {
+                    alert("Please login before rating.");
+                    return false;
+                }
+                return true;
+            },
             huid: "381081",
             uid: "54fb0be2a925ffb881dbeca0d908b038",
             source: "website",
@@ -15,15 +23,20 @@
         });
         RW.render();
     };
-        // Append Rating-Widget JavaScript library.
-    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
-        l = d.location, ck = "Y" + t.getFullYear() +
-        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+    // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0],
+        id = "rw-js",
+        l = d.location,
+        ck = "Y" + t.getFullYear() +
+        "M" + t.getMonth() + "D" + t.getDate(),
+        p = l.protocol,
         f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
         a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
     if (d.getElementById(id)) return;
     rw = d.createElement(e);
-    rw.id = id; rw.async = true; rw.type = "text/javascript";
+    rw.id = id;
+    rw.async = true;
+    rw.type = "text/javascript";
     rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
     s.parentNode.insertBefore(rw, s);
-    }(document, new Date(), "script", "rating-widget.com/"));
+}(document, new Date(), "script", "rating-widget.com/"));
