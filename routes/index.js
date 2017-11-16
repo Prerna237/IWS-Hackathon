f1fa56a7c781951b65a7980b25a076c09a83cd7d
Saketh Vallakatla <saketh21v@gmail.com>
2017-11-17 00:13:24 +0530

    Added moderator check

--- routes/index.js
+++ routes/index.js
@@ -204,17 +204,17 @@ router.get('/threadsByStars/:userName', (req, res) => {
 router.use('/analytics', (req, res, next) => {
     console.log("Request by: " + req.session.userName);
     console.log("Type: " + req.session.moderator);
-    // if (req.session.moderator) {
+    if (req.session.moderator) {
         // Need to fill object
         // res.end(pageHandlers.analyticsPage({}));
         db.getCategoryAnalytics((details) => {
             res.end(pageHandlers.analyticsPage(details));
         });
-    // } else {
-    //     res.end(pageHandlers.errorPage({
-    //         status: 404
-    //     }));
-    // }
+    } else {
+        res.end(pageHandlers.errorPage({
+            status: 404
+        }));
+    }
 });
 
 module.exports = router;
