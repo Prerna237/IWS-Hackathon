48bcf4c9c39fbbf3bc5707a3b7cd5fd51d0e11f8
samarjoypandit <psamarjoy@gmail.com>
2017-11-26 17:50:06 +0530

    thread page changes

--- public/js/thread_page.js
+++ public/js/thread_page.js
@@ -121,6 +121,7 @@ function UpdateSideBox(category) {
             for (var o = 0; o < 5; o++) {
                 data += '<div class="row"><div class="col-2"><a class="user" id="' + document.categories[o].userName + '" style="cursor:pointer;"><canvas class="demo" title="' + document.categories[o].userName + '"alt="Pranjal" style="width:34px; height:34px; margin:-12px 10px; border-radius:50%;"></canvas></a></div><div class="col-10 " style="padding-left: 0px"><a href="' + document.categories[o].id + '">' + document.categories[o].title + '</a></div></div><hr>';
             }
+            $('#sidebox').append('&nbsp;&nbsp;');
             $('#sidebox').append(data);
              $(".demo").letterpic();
         }
